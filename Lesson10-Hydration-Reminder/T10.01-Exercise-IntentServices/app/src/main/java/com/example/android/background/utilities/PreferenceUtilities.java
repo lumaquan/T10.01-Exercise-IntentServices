464d7653df074e0d449cb76da26283b787c2156a
Luis M. Melendez <cogane7955@gmail.com>
2020-05-04 07:48:24 -0500

    Exercise T10.01 Complete! Add IntentService

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/utilities/PreferenceUtilities.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/utilities/PreferenceUtilities.java
@@ -1,27 +1,10 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.example.android.background.utilities;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-/**
- * This class contains utility methods which update water and charging counts in SharedPreferences
- */
 public final class PreferenceUtilities {
 
     public static final String KEY_WATER_COUNT = "water-count";
@@ -38,8 +21,7 @@ public final class PreferenceUtilities {
 
     public static int getWaterCount(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        int glassesOfWater = prefs.getInt(KEY_WATER_COUNT, DEFAULT_COUNT);
-        return glassesOfWater;
+        return prefs.getInt(KEY_WATER_COUNT, DEFAULT_COUNT);
     }
 
     synchronized public static void incrementWaterCount(Context context) {
@@ -50,7 +32,6 @@ public final class PreferenceUtilities {
     synchronized public static void incrementChargingReminderCount(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         int chargingReminders = prefs.getInt(KEY_CHARGING_REMINDER_COUNT, DEFAULT_COUNT);
-
         SharedPreferences.Editor editor = prefs.edit();
         editor.putInt(KEY_CHARGING_REMINDER_COUNT, ++chargingReminders);
         editor.apply();
@@ -58,7 +39,15 @@ public final class PreferenceUtilities {
 
     public static int getChargingReminderCount(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        int chargingReminders = prefs.getInt(KEY_CHARGING_REMINDER_COUNT, DEFAULT_COUNT);
-        return chargingReminders;
+        return prefs.getInt(KEY_CHARGING_REMINDER_COUNT, DEFAULT_COUNT);
     }
+
+    public static void registerSharedPreferencesListener(Context context, SharedPreferences.OnSharedPreferenceChangeListener listener) {
+        PreferenceManager.getDefaultSharedPreferences(context).registerOnSharedPreferenceChangeListener(listener);
+    }
+
+    public static void unregisterSharedPreferencesListener(Context context, SharedPreferences.OnSharedPreferenceChangeListener listener) {
+        PreferenceManager.getDefaultSharedPreferences(context).unregisterOnSharedPreferenceChangeListener(listener);
+    }
+
 }
